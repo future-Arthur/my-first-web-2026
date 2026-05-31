8f9916106f7b7c73eb22bf499011f061a0ae899b
future-Arthur <johntaqui123@gmail.com>
2026-05-31 13:23:51 +0800

    commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+let buttons = document.querySelectorAll(".keys") 
+
+
+for(i = 0; i < buttons.length;i++){
+
+buttons[i].style.fontWeight = "bold";
+buttons[i].style.fontFamily = "arial";
+
+}
+
