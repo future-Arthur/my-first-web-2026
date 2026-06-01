c674b235d832b4049571f8cc0e823c12c077877b
future-Arthur <johntaqui123@gmail.com>
2026-06-01 22:02:34 +0800

    commit

--- script.js
+++ script.js
@@ -8,71 +8,81 @@
 
 let computerMove = Math.random();
 let computerPick = "";
+let result = "";
 
 function rock(){
-    if(computerMove >= 0 && computerMove < 1/3){
-    computerPick  = "rock";
+        if(computerMove >= 0 && computerMove < 1/3){
+    computerPick  = "Rock";
 }
     else if(computerMove >= 1/3 && computerMove < 2/3){
-    computerPick = "paper"
+    computerPick = "Paper"
 }
     else if(computerMove >= 2/3 && computerMove < 1){
-    computerPick =  "scissor"
+    computerPick =  "Scissor"
 }
-    if(computerPick === "rock"){
-        alert(`Computer Picked ${computerPick} its a Tie`);
+        if(computerPick === "Rock"){
+            result = "Tie";
+        alert(`Computer Picked ${computerPick} its a ${result}`);
     }
-    else if(computerPick === "paper"){
-        alert(`Computer Picked ${computerPick} You Lose`)
+    else if(computerPick === "Paper"){
+        result = "Lose";
+        alert(`Computer Picked ${computerPick} You ${result}`)
 
     }
-    else if(computerPick === "scissor"){
-        alert(`Computer Picked ${computerPick} You Win`)
+    else if(computerPick === "Scissor"){
+        result = "Win";
+        alert(`Computer Picked ${computerPick} You ${result}`)
     }
 
 }
 function paper(){
-     if(computerMove >= 0 && computerMove < 1/3){
-    computerPick  = "rock"
+        if(computerMove >= 0 && computerMove < 1/3){
+    computerPick  = "Rock"
 }
     else if(computerMove >= 1/3 && computerMove < 2/3){
-    computerPick = "paper"
+    computerPick = "Paper"
 }
     else if(computerMove >= 2/3 && computerMove < 1){
-    computerPick = "scissor"
+    computerPick = "Scissor"
 }
-    if(computerPick === "rock"){
-        alert(`Computer Picked ${computerPick} You win`);
+        if(computerPick === "Rock"){
+            result = "win"
+            alert(`Computer Picked ${computerPick} You ${result}`);
     }
-    else if(computerPick === "paper"){
-        alert(`Computer Picked ${computerPick} Its a Tie`)
+    else if(computerPick === "Paper"){
+            result = "Tie"
+            alert(`Computer Picked ${computerPick} Its a ${result}`)
 
     }
-    else if(computerPick === "scissor"){
-        alert(`Computer Picked ${computerPick} You Lose`)
+    else if(computerPick === "Scissor"){
+            result = "Lose"
+            alert(`Computer Picked ${computerPick} You ${result}`)
     }
 
 }
 
 function scissor(){
-    if(computerMove >= 0 && computerMove < 1/3){
-    computerPick  = "rock"
+        if(computerMove >= 0 && computerMove < 1/3){
+    computerPick  = "Rock"
 }
     else if(computerMove >= 1/3 && computerMove < 2/3){
-    computerPick =  "paper"
+    computerPick =  "Paper"
 }
     else if(computerMove >= 2/3 && computerMove < 1){
-    computerPick =  "scissor"
+    computerPick =  "Scissor"
 }
-if(computerPick === "rock"){
-        alert(`Computer Picked ${computerPick} You Lose`);
+        if(computerPick === "Rock"){
+            result = "Lose"
+        alert(`Computer Picked ${computerPick} You ${result}`);
     }
-    else if(computerPick === "paper"){
-        alert(`Computer Picked ${computerPick} You Win`)
+    else if(computerPick === "Paper"){
+        result = "win"
+        alert(`Computer Picked ${computerPick} You ${result}`)
 
     }
-    else if(computerPick === "scissor"){
-        alert(`Computer Picked ${computerPick} Its a Tie`)
+    else if(computerPick === "Scissor"){
+        result = "Tie"
+        alert(`Computer Picked ${computerPick} Its a ${result}`)
     }
 }
 
